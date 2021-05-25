794632b1c6d627de2116db97e4788658e9391005
Swapnil Vedpathak <swapnilvedpathak.sv@gmail.com>
2021-05-26 02:31:14 +0530

    Update PDF

--- src/app/calibration-certificate/calibration-certificate.component.ts
+++ src/app/calibration-certificate/calibration-certificate.component.ts
@@ -1,6 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calibration-certificate',
@@ -12,78 +10,15 @@ export class CalibrationCertificateComponent {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'action'];
   dataSource = ELEMENT_DATA;
 
-  @ViewChild('htmlData')
-  htmlData!: ElementRef;
-
-  USERS = [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "email": "sincere@april.biz",
-      "phone": "1-770-736-8031 x56442"
-    },
-    {
-      "id": 2,
-      "name": "Ervin Howell",
-      "email": "shanna@melissa.tv",
-      "phone": "010-692-6593 x09125"
-    },
-    {
-      "id": 3,
-      "name": "Clementine Bauch",
-      "email": "nathan@yesenia.net",
-      "phone": "1-463-123-4447",
-    },
-    {
-      "id": 4,
-      "name": "Patricia Lebsack",
-      "email": "julianne@kory.org",
-      "phone": "493-170-9623 x156"
-    },
-    {
-      "id": 5,
-      "name": "Chelsey Dietrich",
-      "email": "lucio@annie.ca",
-      "phone": "(254)954-1289"
-    },
-    {
-      "id": 6,
-      "name": "Mrs. Dennis",
-      "email": "karley@jasper.info",
-      "phone": "1-477-935-8478 x6430"
-    }
-  ];
-
   constructor() { }
 
-  public openPDF():void {
-    const DATA = document.getElementById('htmlData') as HTMLCanvasElement;
-
-    html2canvas(DATA).then(canvas => {
-
-        let fileWidth = 208;
-        let fileHeight = canvas.height * fileWidth / canvas.width;
-
-        const FILEURI = canvas.toDataURL('image/png')
-        let PDF = new jsPDF('p', 'mm', 'a4');
-        let position = 0;
-        PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
-
-        PDF.save('angular-demo.pdf');
-    });
+  ngOnInit(): void {
   }
 
+
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-
-const ELEMENT_DATA : PeriodicElement[] = [
+const ELEMENT_DATA  = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
--- src/app/list-of-certificate/list-of-certificate.component.ts
+++ src/app/list-of-certificate/list-of-certificate.component.ts
@@ -3,6 +3,16 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { RootService } from '../root.service';
+import { Canvas, Columns, Line, PdfMakeWrapper } from 'pdfmake-wrapper';
+import { Txt } from 'pdfmake-wrapper';
+
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
+PdfMakeWrapper.setFonts(pdfFonts);
+
+const pdf = new PdfMakeWrapper();
 
 @Component({
   selector: 'app-list-of-certificate',
@@ -22,6 +32,9 @@ export class ListOfCertificateComponent {
 
   dataSource = new MatTableDataSource(this.collection);
   displaySpinner = false;
+  certificateData: any = [];
+  pdfData: any;
+  generatedPDF: any;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -46,42 +59,233 @@ export class ListOfCertificateComponent {
       console.log('this.dataSourcekjhjklhkljhk', this.dataSource);
     });
   }
+  onRowClicked(row: any) {
+    console.log('Row clicked: ', row);
+    this.certificateData = row;
+    console.log('certificateData', this.certificateData);
+  }
+
+  // ///////////////////////////////////////////////////////////////////// PDF // /////////////////////////////////////////////////////////////////////
+
+  async generatePdf() {
+    pdf.pageMargins([ 40, 60, 40, 60 ]);
+    this.body();
+    pdf.add(
+      pdf.ln(1)
+  );
+    this.header();
+
+
+
+
+    // var dd = {
+    //   content: [
+
+    //     {
+    //         columns: [
+    //           {
+    //             text: 'M/s, Nviro Development Solutions \n F-7/5, MIDC Chikalthana, Near Vertex Advertising, \n Naregaon Main Road, Aurangabad-431006',
+    //             fontSize: 10
+    //           },
+    //           {
+    //             text: 'Certificate No \n Date Of Calibration \n Next Calibration Date \n Calibration Method/ Ref',
+    //             fontSize: 10
+    //           },
+    //           {
+    //             text: ': 2021/01/002 \n : 18/01/2021 \n : 17/01/2022 \n : NDS-023/NA ',
+    //             fontSize: 10,
+    //             alignMent: 'right'
+    //           }
+    //         ]
+    //     },
+    //     {
+    //       text: 'CALIBRATION CERTIFICATE',
+    //       fontSize : 20,
+    //       style: 'header'
+    //     },
+    //     {
+    //       layout: 'lightHorizontalLines',
+    //       table: {
+    //         // headers are automatically repeated if the table spans over multiple pages
+    //         // you can declare how many rows should be treated as headers
+    //         headerRows: 1,
+    //         widths: [ '*', 'auto', 100, '*' ],
+
+    //         body: [
+
+    //           [{ text: 'First', bold: true }, { text: 'Second', bold: true }, { text: 'Third', bold: true }, { text: 'The last one', bold: true }],
+    //           [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+    //           [ 'Val 2', 'Val 2', 'Val 3', 'Val 4' ]
+    //         ]
+    //       }
+    //     },
+
+    //   ],
+    //   styles: {
+    //     header: {
+    //       fontSize: 22,
+    //       bold: true
+    //     },
+    //     anotherStyle: {
+    //       italics: true
+    //     },
+    //   }
+
+    // };
+
+    // const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+    pdf.create().download();
+  }
+
+
+
+
+  body(){
+    // pdf.add(
+    //   new Columns([ 'Hello', 'world' ]).end
+    // )
+    pdf.pageMargins([ 40, 60, 40, 60 ]);
+
+    pdf.add(
+      new Columns([
+        'M/s, Nviro Development Solutions \n F-7/5, MIDC Chikalthana, Near Vertex Advertising, \n Naregaon Main Road, Aurangabad-431006', 'Certificate No \n Date Of Calibration \n Next Calibration Date \n Calibration Method/ Ref',
+        ': 2021/01/002 \n : 18/01/2021 \n : 17/01/2022 \n : NDS-023/NA '
+      ]).alignment('left').fontSize(10).end
+    );
+
+    pdf.add(
+      new Canvas([
+          new Line([0,360], [490, 360]).end
+      ]).end
+  );
+    // pdf.add(
+    //   new Columns([
+    //     'Certificate No \n Date Of Calibration \n Next Calibration Date \n Calibration Method/ Ref',
+    //     ': 2021/01/002 \n : 18/01/2021 \n : 17/01/2022 \n : NDS-023/NA ',
+    //   ]).columnGap(10).alignment('left').end
+    // );
+  }
+
+  header() {
+    pdf.pageMargins(40);
+    pdf.add(new Txt('CALIBRATION CERTIFICATE').alignment('center').bold().fontSize(20).end);
+    }
+  // generatePDF(action = 'open') {
+  //   console.log('invoice', this.certificateData);
+  //   const docDefinition = {
+  //     info: {
+  //       title: "this.pdfData.title",
+  //       author: "this.pdfData.author",
+  //       subject: "this.pdfData.subject",
+  //       keywords: "this.pdfData.keywords",
+  //       creator: "this.pdfData.creator",
+  //       creationDate: new Date(),
+  //     },
+  //     pageSize: "A4",
+  //     pageOrientation: "landscape",
+  //     pageMargins: [40, 60, 40, 60],
+  //     content: [
+  //       {
+  //         text: 'ELECTRONIC SHOP',
+  //         fontSize: 16,
+  //         alignment: 'center',
+  //         color: '#047886',
+  //       },
+  //       {
+  //         text: 'INVOICE',
+  //         fontSize: 20,
+  //         bold: true,
+  //         alignment: 'center',
+  //         decoration: 'underline',
+  //         color: 'skyblue',
+  //       },
+  //       {
+  //         text: 'Customer Details',
+  //         style: 'sectionHeader',
+  //       },
+  //       {
+  //         columns: [
+  //           [
+  //             {
+  //               text: this.certificateData.customerName,
+  //               bold: true,
+  //             },
+  //             { text: this.certificateData.address },
+  //             { text: this.certificateData.email },
+  //             { text: this.certificateData.contactNo },
+  //           ],
+  //           [
+  //             {
+  //               text: `Date: ${new Date().toLocaleString()}`,
+  //               alignment: 'right',
+  //             },
+  //             {
+  //               text: `Bill No : ${(Math.random() * 1000).toFixed(0)}`,
+  //               alignment: 'right',
+  //             },
+  //           ],
+  //         ],
+  //       },
+  //       {
+  //         text: 'Order Details',
+  //         style: 'sectionHeader',
+  //       },
+  //       {
+  //         table: {
+  //           headerRows: 1,
+  //           widths: ['*', 'auto', 'auto', 'auto'],
+  //           body: [
+  //             ['Product', 'Price', 'Quantity', 'Amount'],
+  //             ...this.certificateData.products,
+  //           ],
+  //           // .map(p => ([p.name, p.price, p.qty, (p.price*p.qty).toFixed(2)])),
+  //           // [{text: 'Total Amount', colSpan: 3}, {}, {}, this.certificateData.products.reduce((sum:any, p:any)=> sum + (p.qty * p.price), 0).toFixed(2)]
+  //         },
+  //       },
+  //       {
+  //         text: 'Additional Details',
+  //         style: 'sectionHeader',
+  //       },
+  //       {
+  //         text: this.certificateData.calibration_method_ref_IS,
+  //         margin: [0, 0, 0, 15],
+  //       },
+  //       {
+  //         columns: [
+  //           [{ qr: `${this.certificateData.customerName}`, fit: '50' }],
+  //           [{ text: 'Signature', alignment: 'right', italics: true }],
+  //         ],
+  //       },
+  //       {
+  //         text: 'Terms and Conditions',
+  //         style: 'sectionHeader',
+  //       },
+  //       {
+  //         ul: [
+  //           'Order can be return in max 10 days.',
+  //           'Warrenty of the product will be subject to the manufacturer terms and conditions.',
+  //           'This is system generated invoice.',
+  //         ],
+  //       },
+  //     ],
+  //     styles: {
+  //       sectionHeader: {
+  //         bold: true,
+  //         decoration: 'underline',
+  //         fontSize: 14,
+  //         margin: [0, 15, 0, 15],
+  //       },
+  //     },
+  //   };
+
+  //  this.generatedPDF = pdfMake.createPdf(docDefinition);
+
+  //   // if(action==='download'){
+
+  //   // }else if(action === 'print'){
+  //   //   pdfMake.createPdf(docDefinition).print();
+  //   // }else{
+  //   //   pdfMake.createPdf(docDefinition).open();
+  //   // }
+  // }
 }
-
-//   displayedColumns = ['id', 'moneyPaidBy', 'toWhomMoneyPaid', 'category', 'actions'];
-//
-//   displaySpinner = false;
-//   showTable = false;
-
-//   dataSource:any;
-
-//   // ngAfterViewInit() {
-//   //   this.dataSource.paginator = this.paginator;
-//   // }
-
-//
-
-//   /
-
-//   onRowClicked(row:any) {
-//     // console.log('Row clicked: ', row);
-// }
-//   openDialog(row: any) {
-//     console.log('Row clicked', row);
-//     // const dialog = this.dialog.open(UpdateExpenceComponent, {
-//     //    height: "600px", width: "500px" ,
-//     //   // Can be closed only by clicking the close button
-//     //   disableClose: true,
-//     //   data: row
-//     // });
-//   }
-
-//   deleteCurrentExpence(item:any){
-//     // console.log("runhsdughfdjghu",item)
-//     // console.warn("swapnil",this.collection)
-//     // this.collection.splice(item-1, 1)
-
-//     // this.root.deleteCurrentExpence(id, item).subscribe((result)=>{
-//     //   console.warn(result)
-//     // })
-//   }
