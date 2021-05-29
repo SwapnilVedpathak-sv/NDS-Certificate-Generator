c88f2333ac1e7398e295df7f6bc4d0bcf8f0b4f3
Swapnil Vedpathak <swapnilvedpathak.sv@gmail.com>
2021-05-30 03:17:19 +0530

    Update PDF

--- src/app/list-of-certificate/list-of-certificate.component.ts
+++ src/app/list-of-certificate/list-of-certificate.component.ts
@@ -3,7 +3,6 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { RootService } from '../root.service';
-import { jsPDF } from 'jspdf';
 import { data } from '../configuration/image-dataURI.config'
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
@@ -60,12 +59,16 @@ export class ListOfCertificateComponent {
     });
   }
 
-  onRowClicked(row: any){ 
-    this.certificateData = row;
+  onRowClicked(rowData: any){
+    this.certificateData = rowData;
+    this.generatePDF("open" , rowData)
   }
 
 
-  generatePDF(action = 'open') {
+  generatePDF(action, rowData) {
+    const dateOfCalibration = rowData.date_of_calibration.slice(0,10);
+    const nextCalibrationDue = rowData.next_calibration_due.slice(0,10);
+    console.log(dateOfCalibration)
     let docDefinition = {
       content: [
         //Header Column
@@ -85,7 +88,7 @@ export class ListOfCertificateComponent {
               fontSize: 10
             },
             {
-              text: ': 2021/01/002 \n : 18/01/2021 \n : 17/01/2022 \n : NDS-023/NA ',
+              text: `: ${rowData.certificate_no} \n :${dateOfCalibration} \n :${nextCalibrationDue} \n : ${rowData.calibration_method_ref_IS}`,
               fontSize: 10,
               alignMent: 'right'
             }
@@ -95,28 +98,205 @@ export class ListOfCertificateComponent {
         },
         //Horizontal Line using SVG
         {
-          svg: `<svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="1200px" height="5px" xml:space="preserve">
+          svg: `<svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="500px" height="5px" xml:space="preserve">
           <path class="path2" fill="#000" stroke-width="3" stroke="#000" d="M0 0 l1120 0"/>
         </svg>`,
-          margin: [ 5, 0, 5, 0]
+          margin: [ 5, 5, 5, 0]
+        },
+        {
+          text: "\n"
         },
         {
           text: 'CALIBRATION CERTIFICATE',
-          fontSize: 18,
+          fontSize: 20,
           bold: true,
           alignment: 'center',
           margin: [ 5, 0, 5, 0]
         },
         {
-          text: 'Ambient Temp  :'
-        }
+          text: "\n"
+        },
+        {
+          columns: [
+            {
+              text: `Ambient Temp \n Relative Humidity`,
+              fontSize: 12
+            },
+            {
+              text: `: ${rowData.ambient_temp} \n : ${rowData.relative_humidity}`,
+              fontSize: 12,
+              alignMent: 'right'
+            },
+            {
+              text: `Location of Calibration \n Condition of Instruments`,
+              fontSize: 12
+            },
+            {
+              text: `: ${rowData.location_of_calibration} \n : Ok`,
+              fontSize: 12
+            },
+          ]
+        },
+        {
+          text: "\n"
+        },
+        {
+          columns: [
+            {
+              text: `Customer Name`,
+              fontSize: 15,
+              bold:true
+            },
+            {
+              text: `: ${rowData.customer_name}`,
+              fontSize: 12,
+              alignMent: 'right'
+            }, {
+              text: `Customer Address`,
+              fontSize: 15,
+              bold:true
+            },
+            {
+              text: `: ${rowData.customer_address}`,
+              fontSize: 12
+            },
+          ]
+        },
+        {
+          text: "\n"
+        },
+        {
+          text:'Instrument Details',
+          fontSize: 15,
+          bold:true
+        },
+        {
+          text: "\n"
+        },
+        {
+          columns: [
+            {
+              text: `Instrument Name \n Id no. \n Serial no. \n Make/Model`,
+              fontSize: 12
+            },
+            {
+              text: `: ${rowData.instrument_name} \n : ${rowData.instrument_id_no} \n : ${rowData.instrument_serial_no} \n : ${rowData.instrument_make_model}`,
+              fontSize: 12,
+              alignMent: 'right'
+            },
+            {
+              text: `Type \n Range \n Least Count \n Acceptance Criteria`,
+              fontSize: 12
+            },
+            {
+              text: `: ${rowData.instrument_type} \n : ${rowData.instrument_range} \n : ${rowData.instrument_serial_no} \n : ${rowData.instrument_least_count}`,
+              fontSize: 12
+            },
+          ]
+        },
+        {
+          text: "\n"
+        },
+        {
+          text:'Standard Instrument Details',
+          fontSize: 15,
+          bold:true
+        },
+        {
+          text: "\n"
+        },
+        {
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [ '*', '*', '*', '*', '*' ],
+
+            body: [
+              [ 'Instruments', 'Identification No.', 'Certificate No', 'Calibration Date', 'Next Due Date' ],
+              [ `${rowData.standard_instrument}`, `${rowData.standard_instrument_identification_no}`, `${rowData.standard_instrument_certificate_no}`, `${rowData.standard_instrument_calibration_date.slice(0,10)}`, `${rowData.standard_instrument_next_calibration_due.slice(0,10)}` ],
+              [ 'Bold value', 'Val 2', 'Val 3', 'Val 4', 'jkghd' ]
+            ]
+          }
+        },
+        {
+          text: "\n"
+        },
+        {
+          text:'Calibration Results',
+          fontSize: 15,
+          bold:true
+        },
+        {
+          text: "\n"
+        },
+        {
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [ '*', '*', '*', '*', '*'],
+
+            body: [
+              [ 'Calibration Point', 'UUC Reading', 'Standard Reading', 'Error In', 'Error In' ],
+              [ '°C', '°C', '°C', '°C', '%' ],
+              [ `${rowData.calibration_results_calibration_points}`, `${rowData.calibration_results_UUC_reading}`, `${rowData.calibration_results_standard_reading}`, `XYZ`, `xyz` ]
+            ]
+          }
+        },
+        {
+          text: "\n"
+        },
+        {
+            text: `Remark : CALIBRATION FOUND OK`,
+            fontSize: 15,
+            bold:true
+        },
+        {
+          text: "\n"
+        },
+        {
+          text:'Note :',
+          fontSize: 15,
+          bold:true
+        },
+        {
+          text: "\n"
+        },
+        {
+          ol: [
+            'UUC Stands for Unit Under Calibration',
+            'Sandards used for calibration are traceable to National/International standards.',
+            'This certificate refers t othe only for particular itesm/submitted for calibration purpose.',
+            'The Observations /Results reported in this particular certificate are valid at the time of and under state condition of mearuement.',
+            'Readings given above are as on received condition of an insturment.',
+            'This certicate shall not be reroduced except in full without our prior permission in writing.'
+          ]
+        },
+        {
+          text: "\n"
+        },
+        {
+          columns: [
+            {
+              text: `Calibrated By`,
+              fontSize: 15,
+              alignMent: 'center',
+              bold:true
+            },
+            {
+              text: `Authorized By`,
+              fontSize: 15,
+              alignMent: 'center',
+              bold:true
+            }
+          ]
+        },
       ],
       styles: {
         sectionHeader: {
           bold: true,
           decoration: 'underline',
           fontSize: 14,
-          margin: [15, 0, 15, 0]          
+          margin: [15, 0, 15, 0]
         }
       }
     };
@@ -124,9 +304,9 @@ export class ListOfCertificateComponent {
     if(action==='download'){
       pdfMake.createPdf(docDefinition).download();
     }else if(action === 'print'){
-      pdfMake.createPdf(docDefinition).print();      
+      pdfMake.createPdf(docDefinition).print();
     }else{
-      pdfMake.createPdf(docDefinition).open();      
+      pdfMake.createPdf(docDefinition).open();
     }
 
   }
