e44cb9c891e047aaac01da519d8929cdc982acb2
Swapnil Vedpathak <swapnilvedpathak.sv@gmail.com>
2021-06-16 23:23:30 +0530

    Update PDF

--- src/app/list-of-certificate/list-of-certificate.component.ts
+++ src/app/list-of-certificate/list-of-certificate.component.ts
@@ -341,14 +341,182 @@ export class ListOfCertificateComponent {
       }
 
       let dd = {
+        pageSize: 'A4',
+        pageMargins: [40, 40, 40, 85],
         content: [
+          //   {
+          //     svg: `<svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="500px" height="5px" xml:space="preserve">
+          //   <path class="path2" fill="#000" stroke-width="3" stroke="#000" d="M0 0 l1120 0"/>
+          // </svg>`,
+          //     margin: [5, 5, 5, 0]
+          //   },
+          // {
+          //   text: "\n"
+          // },
+          {
+            layout: 'headerLineOnly',
+            table: {
+              widths: ['*'],
+              body: [
+                [{ text: `CALIBRATION CERTIFICATE`, fontSize: 20, bold: true, alignment: 'center' }]
+              ]
+            }
+          },
+          {
+            text: "\n"
+          },
+          {
+            layout: 'noBorders',
+            table: {
+              widths: ['*', '*', 50, '*', '*',],
+              body: [
+                [{ text: `Customer Name`, fontSize: 10, bold: true }, { text: `: ${rowData.customer_name}`, fontSize: 10 }, { text: `` }, { text: `Customer Address`, fontSize: 10, bold: true }, { text: `: ${rowData.customer_address}`, fontSize: 10 }]
+              ]
+            }
+          },
+          {
+            text: "\n"
+          },
+          {
+            layout: 'noBorders',
+            table: {
+              widths: ['*', '*', 50, '*', '*',],
+
+              body: [
+                [{ text: `Ambient Temp`, fontSize: 10 }, { text: `: ${rowData.ambient_temp}`, fontSize: 10 }, { text: `` }, { text: `Certificate No`, fontSize: 10 }, { text: `: ${rowData.certificate_no}`, fontSize: 10 }],
+                [{ text: `Relative Humidity`, fontSize: 10 }, { text: `: ${rowData.relative_humidity}`, fontSize: 10 }, { text: `` }, { text: `Date of Calibration`, fontSize: 10 }, { text: `: ${rowData.date_of_calibration?.slice(0, 10)}`, fontSize: 10 }],
+                [{ text: `Location of Calibration`, fontSize: 10 }, { text: `: ${rowData.location_of_calibration}`, fontSize: 10 }, { text: `` }, { text: `Next Calibration Due`, fontSize: 10 }, { text: `: ${rowData.next_calibration_due?.slice(0, 10)}`, fontSize: 10 }],
+                [{ text: `Condition of Instruments`, fontSize: 10 }, { text: `: OK`, fontSize: 10 }, { text: `` }, { text: `Calibration Method/ Ref IS`, fontSize: 10 }, { text: `: ${rowData.calibration_method_ref_IS}`, fontSize: 10 }],
+              ]
+            }
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: 'Instrument Details',
+            fontSize: 15,
+            bold: true
+          },
+          {
+            text: "\n"
+          },
+          {
+            layout: 'noBorders',
+            table: {
+              widths: ['*', '*', 50, '*', '*',],
+
+              body: [
+                [{ text: `Instrument Name`, fontSize: 10 }, { text: `: ${rowData.instrument_name}`, fontSize: 10 }, { text: `` }, { text: `Least Count`, fontSize: 10 }, { text: `: ${rowData.instrument_least_count}`, fontSize: 10 }],
+                [{ text: `Instrument I.D`, fontSize: 10 }, { text: `: ${rowData.instrument_id_no}`, fontSize: 10 }, { text: `` }, { text: `Acceptance Criteria`, fontSize: 10 }, { text: `: ${rowData.acceptance_criteria}`, fontSize: 10 }],
+                [{ text: `Make/Model`, fontSize: 10 }, { text: `: ${rowData.instrument_make_model}`, fontSize: 10 }, { text: `` }, { text: `Department`, fontSize: 10 }, { text: `: ${rowData.instrument_department}`, fontSize: 10 }],
+                [{ text: `Type`, fontSize: 10 }, { text: `: ${rowData.instrument_type}`, fontSize: 10 }, { text: `` }, { text: `Location`, fontSize: 10 }, { text: `: ${rowData.instrument_location}`, fontSize: 10 }],
+                [{ text: `Range`, fontSize: 10 }, { text: `: ${rowData.instrument_range}`, fontSize: 10 }, { text: `` }, { text: `` }, { text: `` }]
+              ]
+            }
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: 'Standard Instrument Details',
+            fontSize: 15,
+            bold: true
+          },
+          {
+            text: "\n"
+          },
           {
             table: {
               widths: ['*', '*', '*', '*', '*'],
+
               body: [
-                [{ text: `Calibration Point (${rowData.instrument_unit})`, fontSize: 10 }, { text: `UUC Reading (${rowData.instrument_unit})`, fontSize: 10 }, { text: `Standard Reading (${rowData.instrument_unit})`, fontSize: 10 }, { text: `Error In (${rowData.instrument_unit})`, fontSize: 10 }, { text: `Error In (%)`, fontSize: 10 }],
-                ['xyz', 'xyz', 'xyz', 'xyz', 'xyz',]
-                // [`${this.CalibrationPoints}`, `${this.UUCReading}`, `${this.StandardIncresingReading}`, `${this.StandardDecresingReading}`, `${this.ErrorInCIncrement}`, `${this.ErrorInCDecrement}`, `${this.ErrorInPercentIncrement}`, `${this.ErrorInPercentDecrement}`]
+                [{ text: `Instruments`, fontSize: 10 }, { text: `Identification No./Sr.no.`, fontSize: 10 }, { text: `Certificate No`, fontSize: 10 }, { text: `Cal. Date`, fontSize: 10 }, { text: `Valid up to`, fontSize: 10 }],
+                [{ text: `${this.standard_instrument}`, fontSize: 10 }, { text: `${this.standard_instrument_identification_no}`, fontSize: 10 }, { text: `${this.standard_instrument_certificate_no}`, fontSize: 10 }, { text: `${this.standard_instrument_calibration_date?.slice(0, 10)}`, fontSize: 10 }, { text: `${this.standard_instrument_next_calibration_due?.slice(0, 10)}`, fontSize: 10 }]
+              ]
+            }
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: 'Calibration Results',
+            fontSize: 15,
+            bold: true
+          },
+          {
+            text: "\n"
+          },
+          {
+            
+            table: {
+              headerRows: 2,
+              widths: ['*', '*', '*', '*', '*','*','*','*',],
+              body: [
+                [{ text: `Calibration Point (${rowData.instrument_unit})`, fontSize: 10, rowSpan: 2 }, { text: `UUC Reading (${rowData.instrument_unit})`, fontSize: 10, rowSpan: 2 }, { text: `Standard Reading (${rowData.instrument_unit})`, fontSize: 10, colSpan: 2 }, '', { text: `Error In (${rowData.instrument_unit})`, fontSize: 10, colSpan: 2}, '', { text: `Error In (%)`, fontSize: 10, colSpan: 2 }, ''],
+                ['', '', 'Increment', 'Decrement','Increment', 'Decrement','Increment', 'Decrement',],
+                [{ text: `${this.CalibrationPoints}`, fontSize: 10 }, { text: `${this.UUCReading}`, fontSize: 10 }, { text: `${this.StandardIncresingReading}`, fontSize: 10 }, { text: `${this.StandardDecresingReading}`, fontSize: 10 }, { text: `${this.ErrorInCIncrement}`, fontSize: 10 },  { text: `${this.ErrorInCDecrement}`, fontSize: 10 }, { text: `${this.ErrorInPercentIncrement}`, fontSize: 10 }, { text: `${this.ErrorInPercentDecrement}`, fontSize: 10 }]
+              ]
+            }
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: `Remark : CALIBRATION FOUND OK`,
+            fontSize: 10,
+            bold: true
+          },
+          {
+            text: "\n"
+          },
+          {
+            text: 'Note :',
+            fontSize: 12,
+            bold: true
+          },
+          {
+            text: "\n"
+          },
+          {
+            ol: [
+              'UUC Stands for Unit Under Calibration',
+              'Sandards used for calibration are traceable to National/International standards.',
+              'This certificate refers t othe only for particular itesm/submitted for calibration purpose.',
+              'The Observations /Results reported in this particular certificate are valid at the time of and under state condition of mearuement.',
+              'Readings given above are as on received condition of an insturment.',
+              'This certicate shall not be reroduced except in full without our prior permission in writing.'
+            ],
+            fontSize: 10,
+          },
+          {
+            text: "\n"
+          },
+          {
+            table: {
+              heights: [10, 70],
+              widths: ['*', '*'],
+              body: [
+                [{ text: `Calibrated By`, fontSize: 10, bold: true, alignment: 'center' }, { text: `Authorized By`, alignment: 'center', fontSize: 10, bold: true }],
+                [{ text: `` }, { text: `` }]
+              ]
+            },
+            margin: [0, 0, 0, 5]
+          },
+          {
+            table: {
+              widths: ['*'],
+              body: [
+                [{ text: `**** End of Calibration Certificate ****`, fontSize: 10, alignment: 'center' }]
               ]
             }
           }
